09bae840ddd4d4489fece52a06b27d6e2a16a53e
Santiago Salazar <santiagosalazar.dev@gmail.com>
2023-10-28 16:38:44 -0400

    Implementation of react feather, validation for required fields, use of unique ids within items, code optimization, stylization

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -4,33 +4,40 @@ import * as Icon from 'react-feather';
 export const ItemNote = ({value, index, data, setData}) => {
     const [ disabled, setDisabled ] = useState(true)
     const [ colapse, setColapse ] = useState(true)
+
     
     const handleEditItem = (event) => {
         event.preventDefault()
+        const id = event.target.id;
         setDisabled(!disabled)
 
         if (!disabled) {
-            console.log("save");
-
             const form = Object.fromEntries(new FormData(event.target))
             const dayOfModification = new Date()
 
-            const newData = [...data]
-            const actualItem = newData[event.target.id];
+            const dataClone = [...data]
+            const newData = dataClone.map((item) => {
 
-            actualItem.title = form.title;
-            actualItem.description = form.description;
-            actualItem.date = dayOfModification.toLocaleDateString()
-            
+                if(item.id === id){
+                    item.title = form.title;
+                    item.description = form.description;
+                    item.date = dayOfModification.toLocaleDateString()
+                    return item
+                } else {
+                    return item
+                }
+            })
+
             setData(newData)
         }
     }
 
     const deleteItem = (event, index) => {
         event.preventDefault() 
-
-        const newData = [...data]
-        newData.splice(Number(index), 1)
+        let newData = [...data]
+        newData = newData.filter((item) => {
+            return item.id !== index
+        })
         setData(newData)
     }
     
@@ -44,7 +51,7 @@ export const ItemNote = ({value, index, data, setData}) => {
                     </button>
                 </div>
                 :
-                <form onSubmit={handleEditItem} id={index}>
+                <form onSubmit={handleEditItem} id={value.id}>
                     <input type="text" name="title" id="" defaultValue={value.title} style={{ pointerEvents: disabled ? 'none' : 'auto' }}/>
                     <textarea rows="" cols="" name="description" id="description" defaultValue={value.description} style={{ pointerEvents: disabled ? 'none' : 'auto' }}/>
                     <h2 className="date-text">{value.date}</h2>
@@ -52,21 +59,20 @@ export const ItemNote = ({value, index, data, setData}) => {
                     <div className="buttons">
                         <div className="actions">
                             <button>{disabled ? 'Editar' : 'Guardar'}</button>
-                            <button id={index} onClick={(event) => deleteItem(event, index)}>Eliminar</button>
+                            <button onClick={(event) => deleteItem(event, value.id)}>Eliminar</button>
                         </div>
                         <button className="btn-control-item" onClick={() => setColapse(true)}>
                             <Icon.Minimize />
                         </button>
-
                     </div>
                 </form>
-
             }
         </li>
     )
 }
 
 export default function ListItems({data, setData, switchMode, keyWord}) {
+
     const filteredData = data.filter((item, key) => {
         if(item.title.includes(keyWord) || item.description.includes(keyWord)){
             return <ItemNote value={item} index={key} data={data} setData={setData} key={`key-${key}-${JSON.stringify(item)}`}/>
@@ -76,8 +82,11 @@ export default function ListItems({data, setData, switchMode, keyWord}) {
     return (
         <>
         <div className="ListItems">
-            <h1>Historial de notas</h1>
-            <ul>
+            <h1 className="title">
+                <Icon.List />
+                Historial de notas
+            </h1>
+            <ul style={{ padding: 0 }}>
                 {filteredData.length === 0 ?
                     <p>Ups, parece que no se han encontrado notas.</p>
                     :
--- src/components/NewItem.jsx
+++ src/components/NewItem.jsx
@@ -1,16 +1,39 @@
+import * as Icon from 'react-feather';
+
 const NewItem = ({data, setData, switchMode}) => {
+    
+    const validateNewItem = (title) => {
+        return data.find((item) => {
+            return item.title === title
+        })
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault()
         const form = new FormData(event.target)
+        const formFromEntries = Object.fromEntries(form)
         const dateOfCreation = new Date()
+        form.append("id", formFromEntries.title.toLowerCase().replaceAll(' ', '_'));
         form.append("date", dateOfCreation.toLocaleDateString());
-        setData([...data, Object.fromEntries(form)]);
+
+        if (formFromEntries.title !== '' && formFromEntries.description !== '') {
+            if(!validateNewItem(formFromEntries.title)){
+                setData([...data, formFromEntries]);
+            } else {
+                alert(`La nota con el nombre ${formFromEntries.title} ya existe, por favor seleccione un nombre diferente.`)
+            }
+        } else {
+            alert('Debes introducir un titulo y una descripcion como minimo para añadir una nota.')
+        }
     }
+
 
     return (
         <div className='NewItem'>
-            <h1>Nueva nota</h1>
+            <h1 className='title'>
+                <Icon.FilePlus />
+                Nueva nota
+            </h1>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="title">
                     <span>Titulo: </span>
@@ -22,7 +45,7 @@ const NewItem = ({data, setData, switchMode}) => {
                 </label>
                 <label htmlFor="color">
                     <span>Color de la nota: </span>
-                    <input type="color" name="color" id="color" />
+                    <input type="color" name="color" id="color" defaultValue={'#2a64f6'}/>
                 </label>
                 <button>
                     Guardar Nota
